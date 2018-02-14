d53ca6e325032359de40a1288e95eef6434e7033
willblev <willblev@gmail.com>
2018-02-14 11:51:24 +0100

    added user input function, its working

--- photo_booth_backend.py
+++ photo_booth_backend.py
@@ -8,15 +8,53 @@ modes=['1x4','single']
 fonts_list=['goingtodogreatthings']
 # directories
 scripts_directory="/home/pi/can_pages_photobooth"
-photos_directory="/home/pi/Pictures/"+time.strftime("%d-%m-%Y")+"_"+novios.names[0]+"_"+novios.names[1]
+photos_directory="/home/pi/Pictures/"+time.strftime("%d-%m-%Y")
 temp_photos_directory="/home/pi/Pictures/temp"
 
 #pre-formatted text strings
 date_text_string="%s %s %s" % (time.strftime("%d").lstrip("0"),time.strftime("%B"),time.strftime("%Y"))	
 date_number_string="%s %s %s" % (time.strftime("%d").lstrip("0"),time.strftime("%B"),time.strftime("%Y"))	
-photo_text_string="%s" % "hahaha"
+photo_text_string=''
 
 ###### Define functions
+def get_text_string_from_user():
+	confirmation1=''
+	confirmation2=''
+	user_input1 = raw_input("\n\n\nPor favor, introduce el texto (30 caracteres max) que aparecera en la primera linea. Cuando este, pulse ENTER\n\n")
+
+	while len(user_input1) >30 or confirmation1!='SI':
+		if len(user_input1) >30:
+			user_input1 = raw_input("\nOops- has introducido mas de 30 caracteres (eran %s)! Por favor, vuelve a introduce el texto para la primera linea. Cuando este, pulse ENTER\n\n" % len(user_input1))
+		elif len(user_input1) <=30 and confirmation1!='SI':
+			print("\nEl texto que has introducido es (%s caracteres):\n%s\n" % (len(user_input1),user_input1))
+			confirmation1=raw_input("\nEsta todo correcto? Introduce SI si todo esta correcto y quieres seguir, o introduce NO si quieres volver a introducir el texto.\n\n")
+			confirmation1=confirmation1.upper()
+			if confirmation1!='SI':
+				user_input1 = raw_input("\n\n\nPor favor, introduce el texto (30 caracteres max) que aparecera en la primera linea. Cuando este, pulse ENTER\n\n")
+	user_input2 = raw_input("\n\n\nPor favor, introduce el texto (30 caracteres max) que aparecera en la segunda linea. Cuando este, pulse ENTER\n\n")
+	while len(user_input2) >30 or confirmation2!='SI':
+		if len(user_input2) >30:
+			user_input2 = raw_input("\nOops- has introducido mas de 30 caracteres (eran %s)! Por favor, vuelve a introduce el texto para la segunda linea. Cuando este, pulse ENTER\n\n" % len(user_input2))
+		elif len(user_input2) <=30 and confirmation2!='SI':
+			print("\nEl texto que has introducido es (%s caracteres):\n%s\n" % (len(user_input2),user_input2))
+			confirmation2=raw_input("\nEsta todo correcto? Introduce SI si todo esta correcto y quieres seguir, o introduce NO si quieres volver a introducir el texto.\n\n")
+			confirmation2=confirmation2.upper()
+			if confirmation2!='SI':
+				user_input2 = raw_input("\n\n\nPor favor, introduce el texto (30 caracteres max) que aparecera en la segunda linea. Cuando este, pulse ENTER\n\n")
+
+	photo_text_string="%s\n%s" % (user_input1,user_input2)
+	return photo_text_string
+def clear_temp_directory():
+	"""
+	A function which deletes all the files in the temp directory
+	"""
+	try:
+		command="rm %s/*"%(temp_photos_directory)
+		subprocess.check_output("%s"% command, shell=True)
+		num_captures+=1
+	except subprocess.CalledProcessError as e:
+		print e.output	
+
 
 def capture_pictures(mode):
 	"""
@@ -109,9 +147,8 @@ def create_photo_montage(font,text,mode):
 		print("Please select a correct mode {single, 1x4}")
 
 
-
-		
+text=get_text_string_from_user()	
 
 capture_pictures("1x4")
 
-create_photo_montage(fonts_list[0],"J+M", '1x4')
+create_photo_montage(fonts_list[0],text, '1x4')
--- test.py
+++ test.py
@@ -1,13 +1,43 @@
 import  locale, time, os, subprocess, errno
 temp_photos_directory="/home/william/github/can_pages_photobooth/example_images" 
 font='goingtodogreatthings'
-text="Katie + Will\nCan Pages"
 
 
 ## copy photos from fresh to example
 command="cp  /home/william/github/can_pages_photobooth/example_images/fresh/capture* /home/william/github/can_pages_photobooth/example_images/"  
 print(command)
 subprocess.check_output("%s" % command, shell=True)
+
+def get_text_string_from_user():
+	confirmation1=''
+	confirmation2=''
+	user_input1 = raw_input("\n\n\nPor favor, introduce el texto (30 caracteres max) que aparecera en la primera linea. Cuando este, pulse ENTER\n\n")
+
+	while len(user_input1) >30 or confirmation1!='SI':
+		if len(user_input1) >30:
+			user_input1 = raw_input("\nOops- has introducido mas de 30 caracteres (eran %s)! Por favor, vuelve a introduce el texto para la primera linea. Cuando este, pulse ENTER\n\n" % len(user_input1))
+		elif len(user_input1) <=30 and confirmation1!='SI':
+			print("\nEl texto que has introducido es (%s caracteres):\n%s\n" % (len(user_input1),user_input1))
+			confirmation1=raw_input("\nEsta todo correcto? Introduce SI si todo esta correcto y quieres seguir, o introduce NO si quieres volver a introducir el texto.\n\n")
+			confirmation1=confirmation1.upper()
+			if confirmation1!='SI':
+				user_input1 = raw_input("\n\n\nPor favor, introduce el texto (30 caracteres max) que aparecera en la primera linea. Cuando este, pulse ENTER\n\n")
+	user_input2 = raw_input("\n\n\nPor favor, introduce el texto (30 caracteres max) que aparecera en la segunda linea. Cuando este, pulse ENTER\n\n")
+	while len(user_input2) >30 or confirmation2!='SI':
+		if len(user_input2) >30:
+			user_input2 = raw_input("\nOops- has introducido mas de 30 caracteres (eran %s)! Por favor, vuelve a introduce el texto para la segunda linea. Cuando este, pulse ENTER\n\n" % len(user_input2))
+		elif len(user_input2) <=30 and confirmation2!='SI':
+			print("\nEl texto que has introducido es (%s caracteres):\n%s\n" % (len(user_input2),user_input2))
+			confirmation2=raw_input("\nEsta todo correcto? Introduce SI si todo esta correcto y quieres seguir, o introduce NO si quieres volver a introducir el texto.\n\n")
+			confirmation2=confirmation2.upper()
+			if confirmation2!='SI':
+				user_input2 = raw_input("\n\n\nPor favor, introduce el texto (30 caracteres max) que aparecera en la segunda linea. Cuando este, pulse ENTER\n\n")
+
+	photo_text_string="%s\n%s" % (user_input1,user_input2)
+	return photo_text_string
+
+
+photo_text_string=get_text_string_from_user()
 
 ## perhaps we don't need to crop
 #command="mogrify -gravity Center -crop '70x80%+0+0'  /home/william/github/can_pages_photobooth/example_images/capture*.jpg"  
@@ -20,7 +50,7 @@ print(command)
 subprocess.check_output("%s" % command, shell=True)
 
 ## create label
-command="convert -background white -size 1960x660 -fill black -font '%s' -gravity center  label:'%s'  %s/photobooth_label.jpg" % (font, text, temp_photos_directory)
+command="convert -background white -size 1960x660 -fill black -font '%s' -gravity center  label:'%s'  %s/photobooth_label.jpg" % (font, photo_text_string, temp_photos_directory)
 print(command)
 subprocess.check_output("%s" % command, shell=True)
 
