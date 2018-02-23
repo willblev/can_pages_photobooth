88b94142dfdb277e612da8549f4692c46dc24a8b
willblev <willblev@gmail.com>
2018-02-23 18:36:10 +0100

    working well

--- photo_booth_backend.py
+++ photo_booth_backend.py
@@ -19,10 +19,35 @@ language_dict={		   # dictionary to set the language/locale for each event
 				'ESP':'es_ES.UTF-8', 
 				'CAT':'ca_ES.UTF-8'
 				}
-prompt_text_dict={
-				'ENG':'Press the button to start!', 
-				'ESP':'Presiona el botón para comenzar!', 
-				'CAT':'Premeu el botó per començar!'
+prompt1_text_dict={
+				'ENG':'Press the button to start', 
+				'ESP':'Presiona el botón para comenzar', 
+				'CAT':'Premeu el botó per començar'
+				}
+prompt2_text_dict={
+				'ENG':'Get ready!', 
+				'ESP':'¡Preparate!', 
+				'CAT':"Prepara't!"
+				}
+prompt3_text_dict={
+				'ENG':'Get ready!', 
+				'ESP':'¡Preparate!', 
+				'CAT':"Prepara't!"
+				}
+prompt4_text_dict={
+				'ENG':'Processing photos...', 
+				'ESP':'Procesando las fotos...', 
+				'CAT':"Processant les fotos..."
+				}
+prompt5_text_dict={
+				'ENG':'Printing...', 
+				'ESP':'Imprimiendo...', 
+				'CAT':"Imprimint..."
+				}
+prompt6_text_dict={
+				'ENG':'Please take your photo', 
+				'ESP':'Por favor recoja su foto', 
+				'CAT':"Tria la teva foto"
 				}
 
 # directories
@@ -40,16 +65,20 @@ def wait_for_button_press():
 	"""
 	A function which listens for button presses, then begins the photobooth series of events"
 	"""
+	global mode
+	global language
 	try:
 		GPIO.wait_for_edge(gpio_pin_number, GPIO.FALLING)
-		
+		prompt_screen(prompt2_text_dict[language],120)
 		command0="rm fifo.mjpg"
 		subprocess.check_output("%s"% command0, shell=True)    
 		command1="mkfifo fifo.mjpg"
 		subprocess.check_output("%s"% command1, shell=True)
 		command2="gphoto2 --capture-movie=60 --stdout> fifo.mjpg & omxplayer fifo.mjpg --live"
 		subprocess.check_output("%s"% command2, shell=True)
-		capture_pictures("1x4")
+		prompt_screen(prompt3_text_dict[language],120)
+		time.sleep(1)
+		capture_pictures()
 	except:
 		pass
 
@@ -86,17 +115,21 @@ def clear_temp_directory():
 	except subprocess.CalledProcessError as e:
 		print(e.output)	
 
-
-def capture_pictures(mode):
+	
+def capture_pictures():
 	"""
 	A function which calls gphoto2 to capture the images from the DSLR
 	"""
+	global mode
+	global language
 	clear_temp_directory()
-	if mode=="1x4":
+	
+	if mode=="5x15":
 		num_captures=0	
 		while num_captures<4:
 			for x in range(3,0,-1):
 				print("Taking picture in %s" % x)
+				prompt_screen(str(x),600)
 				time.sleep(1)
 			try:
 				command="gphoto2 --capture-image-and-download --filename %s/capture_%s:%s:%s.jpg"%(temp_photos_directory,time.strftime("%H"),time.strftime("%M"),time.strftime("%S"))
@@ -104,7 +137,7 @@ def capture_pictures(mode):
 				num_captures+=1
 			except subprocess.CalledProcessError as e:
 				print(e.output)	
-	elif mode=="single":
+	elif mode=="10x15":
 		try:
 			command="gphoto2 --capture-image-and-download --filename %s/capture_%s:%s:%s.jpg"%(temp_photos_directory,time.strftime("%H"),time.strftime("%M"),time.strftime("%S"))
 			subprocess.check_output("%s"% command, shell=True)
@@ -121,13 +154,14 @@ def create_photos_dir():
 			print("Created "+photos_directory)
 		except subprocess.CalledProcessError as e:
 			print(e.output)
-			
+
 			
 def create_photo_montage(font,text,mode):
 	"""
 	A function which takes the captured images and creates a file (single shot or 1x4 photo strip) in the appropriate directory
 	"""
-						
+	global language
+	prompt_screen(prompt4_text_dict[language],120)					
 	if mode=="5x15":
 		try:         
 			command="mogrify -resize 960x640 %s/capture*.jpg" % temp_photos_directory
@@ -157,7 +191,9 @@ def create_photo_montage(font,text,mode):
 								try:         
 									command="lp -d Dai_Nippon_Printing_DS40/1x4 %s/temp_montage_final.jpg" % (temp_photos_directory)
 									print(command)
-									subprocess.check_output("%s" % command, shell=True)							
+									subprocess.check_output("%s" % command, shell=True)	
+									prompt_screen(prompt5_text_dict[language],80)					
+						
 								except subprocess.CalledProcessError as e:
 									print(e.output)	
 							except subprocess.CalledProcessError as e:
@@ -182,6 +218,8 @@ def create_photo_montage(font,text,mode):
 				command="lp %s" % (new_file_name)
 				print(command)
 				subprocess.check_output("%s" % command, shell=True)
+				prompt_screen(prompt5_text_dict[language],80)					
+
 				
 			except subprocess.CalledProcessError as e:
 				print(e.output)	
@@ -191,40 +229,37 @@ def create_photo_montage(font,text,mode):
 		print("Please select a correct mode {5x15, 10x15}")
 
 
-def prompt_screen():
-	global language
+
+def prompt_screen(inputText,fontSize):
 	pygame.font.init()
 	WHITE = (255, 255, 255)
 	BLACK = (0, 0, 0)
 	TURQUOISE=(117, 239, 217)
 	PINK=(255, 127, 229)
-
-	FONTSIZE = 80 
-
 	pygame.display.init()
 	size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
 	screen = pygame.display.set_mode(size,pygame.FULLSCREEN)
 	screen.fill(BLACK)        
 	pygame.display.update()
 
-	font = pygame.font.SysFont("DKCosmoStitch", FONTSIZE)
+	font = pygame.font.SysFont("DKCosmoStitch", fontSize)
 	text_color = TURQUOISE
 	led_state = False
 	screen.fill(BLACK)
-	text = font.render(prompt_text_dict[language], 1, text_color)
+	text = font.render(inputText, 1, text_color)
 	textpos = text.get_rect()
 	textpos.center = (pygame.display.Info().current_w/2, pygame.display.Info().current_h/2)
 	screen.blit(text, textpos)
 	pygame.display.flip()
-	time.sleep(2)
+
 
 
 import_config_file("/home/pi/can_pages_photobooth/config.txt")
-
 create_photos_dir()
-prompt_screen()
-#wait_for_button_press()
-#create_photo_montage(font,text, mode)
-
+prompt_screen(prompt1_text_dict[language],80)
+wait_for_button_press()
+create_photo_montage(font,text, mode)
+time.sleep(10)
+prompt_screen(prompt6_text_dict[language])
 
 GPIO.cleanup()
