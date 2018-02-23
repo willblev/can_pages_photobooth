66b967f00f847c147dc7b1a63b278911aa3b756d
willblev <willblev@gmail.com>
2018-02-23 15:36:42 +0100

    working on countdown

---
+++ countdown_test.py
@@ -0,0 +1,42 @@
+import pygame
+import time
+
+pygame.font.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+TURQUOISE=(117, 239, 217)
+PINK=(255, 127, 229)
+
+N_COUNTDOWN = 5
+FONTSIZE = 960 
+
+
+pygame.display.init()
+size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+screen = pygame.display.set_mode(size,pygame.FULLSCREEN)
+
+screen.fill(BLACK)        
+pygame.display.update()
+
+
+
+font = pygame.font.SysFont("DKCosmoStitch", FONTSIZE)
+
+text_color = TURQUOISE
+
+led_state = False
+
+for i in range(N_COUNTDOWN):
+    screen.fill(BLACK)
+    text = font.render(str(N_COUNTDOWN - i), 1, text_color)
+    textpos = text.get_rect()
+    textpos.center = (pygame.display.Info().current_w/2, pygame.display.Info().current_h/2)
+    screen.blit(text, textpos)
+    pygame.display.flip()
+    if i < N_COUNTDOWN - 2:
+        time.sleep(1)
+    else:
+        for j in range(5):
+            time.sleep(.2)
+        
